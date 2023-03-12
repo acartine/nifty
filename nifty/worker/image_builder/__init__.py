aa937be9ee60795351a4854eeb902e751330da2a
acartine <cartine@gmail.com>
2023-03-12 08:00:04 +0100

    simplifying asyncio with interrupt handling

--- nifty/worker/image_builder/__init__.py
+++ nifty/worker/image_builder/__init__.py
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
 from nifty.common.types import Channel
 from nifty.worker.common.asyncio import worker
 from .image_builder import ImageBuilderWorker
 
 worker.start(lambda: ImageBuilderWorker(), src_channel=Channel.image_builder)
-=======
-import asyncio
-import logging
-
-from nifty.common import cfg
-from nifty.common.types import Channel
-from nifty.common import log
-from .image_builder import ImageBuilderWorker
-
-ENRICHMENT_CONFIG_KEY = "enrichment"
-
-log.log_init()
-worker = ImageBuilderWorker()
-logging.getLogger(__name__).debug("launching asyncio")
-asyncio.run(
-    worker.run(
-        src_channel=Channel.trend,
-        listen_interval=cfg.gint(ENRICHMENT_CONFIG_KEY, "listen_interval_sec"),
-    )
-)
->>>>>>> ad9d80f (refactoring for better support for unit tests.)
--- nifty/worker/trend/__init__.py
+++ nifty/worker/trend/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from nifty.common import cfg
 from nifty.common.types import Channel
 from .trend import TrendWorker
@@ -13,26 +12,4 @@ worker.start(
         toplist_bucket_len_sec=cfg.gint(TREND_CONFIG_KEY, "toplist_bucket_len_sec"),
     ),
     src_channel=Channel.trend,
-=======
-import asyncio
-
-from nifty.common import cfg, log
-from nifty.common.types import Channel
-from .trend import TrendWorker
-
-TREND_CONFIG_KEY = "trend"
-
-log.log_init()
-trend = TrendWorker(
-    trend_size=cfg.gint(TREND_CONFIG_KEY, "size"),
-    toplist_interval_sec=cfg.gint(TREND_CONFIG_KEY, "toplist_interval_sec"),
-    toplist_bucket_len_sec=cfg.gint(TREND_CONFIG_KEY, "toplist_bucket_len_sec"),
-)
-
-asyncio.run(
-    trend.run(
-        src_channel=Channel.action,
-        listen_interval=cfg.gint(TREND_CONFIG_KEY, "listen_interval_sec"),
-    )
->>>>>>> ad9d80f (refactoring for better support for unit tests.)
 )
